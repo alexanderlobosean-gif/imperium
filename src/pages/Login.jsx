391d904ad6fcd1347351880c326192ee20326b60
Trade Pal Exchange <trade@pal-exchange.com>
2026-03-29 14:56:44 -0300

    feat: ajustar logos em login e register - remover textos e usar h-26 w-auto
    - Login: logo_p.png h-26 w-auto (removido texto IMPERIUM CLUB)
    - Register: logo_p.png h-26 w-auto (removido texto IMPERIUM CLUB)
    - Padronizar proporções das logos em todo o site
    - Manter apenas logo sem texto adicional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -55,14 +55,9 @@ const Login = () => {
                   <img 
                     src="/logo_p.png" 
                     alt="Imperium Club" 
-                    className="w-20 h-20 mr-3"
+                    className="h-26 w-auto mr-3"
                   />
-                  <div>
-                    <h1 className="text-3xl font-bold bg-gradient-to-r from-yellow-400 to-yellow-600 bg-clip-text text-transparent">
-                      IMPERIUM
-                    </h1>
-                    <p className="text-sm text-gray-400 -mt-1">CLUB</p>
-                  </div>
+                  
                 </div>
               </div>
 
